3ce652d817f208d9094738b0c0a5d6fbfff97e1b
qonstellations <aarush.a.pathak@gmail.com>
2025-12-28 09:58:09 +0530

    initialised project and created structure

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+TO DO
+
+1) Extract datetime object from image metadata
+2) Extract GPS from image metadata
+"""
+
+def main():
+    print("Hello from imageorganiser!")
+
+if __name__ == "__main__":
+    main()
